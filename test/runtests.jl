83eb12b6eb998b08649d408eb4853a8d0da3a57c
Isa Restrepo <isabelrestre@gmail.com>
2016-08-05 16:41:06 -0400

    Implemented basic coo-matrix and contingency statistics

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,12 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+my_tests = ["cooccur.jl"]
+
+println("Running tests:")
+
+for my_test in my_tests
+    @printf " * %s\n" my_test
+    include(my_test)
+end
