fb2cb2483e765f92a65c664f330bce950880afe9
Mary McGrath <m.c.mcgrath13@gmail.com>
2018-11-06 17:25:00 -0500

    Update cooccur.jl

--- test/cooccur.jl
+++ test/cooccur.jl
@@ -5,19 +5,26 @@ using StatsBase
 occur_file = "/Users/isa/Dropbox/BrownAgain/Projects/BCBI/test/occur_sp.jdl"
 labels_file = "/Users/isa/Dropbox/BrownAgain/Projects/BCBI/test/mesh2ind.jdl"
 
-occur = read_sp_occur(occur_file)
-label2ind = read_occur_label_dict(labels_file)
+#Contingency
+x=[1,0,0,1,0,1,1,0,0,1,0,1,1,1,1,1,1,1,0,0,0,0,1,0,1,1,0,0,1,0,1]
+y=[1,1,1,0,0,0,0,1,1,0,0,1,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,0,0]
+
+occur = [x y]
+label2ind = Dict("x" => 1, "y" => 2)
+
+try
+  global occur = read_sp_occur(occur_file)
+  global label2ind = read_occur_label_dict(labels_file)
+catch
+  continue
+end
 
 COO = occur2coo(occur, label2ind)
 
 corr = corrcoef(occur)
 pmi = pmi(COO.coo_matrix, sum(occur))
 
-#Contingency
-x=[1,0,0,1,0,1,1,0,0,1,0,1,1,1,1,1,1,1,0,0,0,0,1,0,1,1,0,0,1,0,1]
-y=[1,1,1,0,0,0,0,1,1,0,0,1,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,0,0,0,0]
 
-occur = [x y]
 
 d = counts(x,y, span(occur))
 stat = chi2_statistic(x,y,span(occur))
